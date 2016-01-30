fb706bfc8c4f2c1512b963fe781298532723dbfc
Sean Stoops <brutimus@gmail.com>
2016-01-29 17:36:07 -0800

    Reworked the UI

--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
 //@codekit-prepend "../bower_components/jquery/dist/jquery.js"
 
 jQuery(document).ready(function($) {
-  var imgContainer = $('#images'),
+  var imgContainer = $('#image'),
       newConfig = {"frames": {}},
       hotSpotStyleForm = $('#hotspotStyle')
       boxXForm = $('#boxXInput'),
       boxYForm = $('#boxYInput'),
-      newConfigForm = $('#newConfig');
+      newConfigForm = $('#newConfig'),
+      scrubber = $('#scrubber'),
+      frames = [],
+      imgPrefix = "https://orgeadvi.sirv.com/";
+
+  scrubber.on('input', function(e){
+    showFrame($(this).val());
+  });
+  $('#previousFrameButton').click(previousFrame);
+  $('#nextFrameButton').click(nextFrame);
+
+  function showFrame(num) {
+    imgContainer.empty().append(frames[num - 1]);
+  }
+
+  function nextFrame(){
+    index = parseInt(scrubber.val());
+    if (index < parseInt(scrubber.attr('max'))) {
+      showFrame(index + 1);
+      scrubber.val(index + 1);
+    }
+  }
+
+  function previousFrame(){
+    index = parseInt(scrubber.val());
+    if (index > 1) {
+      showFrame(index - 1);
+      scrubber.val(index - 1);
+    }
+  }
 
   $('#spinFileForm').submit(function(e){
     e.preventDefault();
     var url = $('#spinFileInput').val();
+    var parser = document.createElement('a');
+    parser.href = url;
+    path = parser.pathname.split('/')
+    imgPrefix = imgPrefix + path.slice(1, path.length - 1).join('/') + '/';
     $.ajax({
       url: url,
       jsonp: "callback",
@@ -19,21 +52,26 @@ jQuery(document).ready(function($) {
       // Work with the response
       success: function( response ) {
         $('#spinFile').val(JSON.stringify(response, null, '\t'));
+        scrubber.attr('max', Object.keys(response.layers[1]).length);
         $.each(response.layers[1], function(k, v){
-          var div = $('<div>').appendTo(imgContainer);
-          $('<img>')
-          .attr('src', '//orgeadvi.sirv.com/vanimation/' + v)
-          .attr('data-frame', k)
-          .appendTo(div)
-          $('<p>').appendTo(div);
+          var i = new Image();
+          i.src = imgPrefix + v;
+          frames.push(i);
         })
+        showFrame(1);
+        //   var div = $('<div>').appendTo(imgContainer);
+        //   $('<img>')
+        //   .attr('src', '//orgeadvi.sirv.com/vanimation/' + v)
+        //   .attr('data-frame', k)
+        //   .appendTo(div)
+        //   $('<p>').appendTo(div);
+        // })//https://orgeadvi.sirv.com/vanimation/vanimation.spin
 
-        $('img').click(function(e) {
+        imgContainer.click(function(e) {
           var offset = $(this).offset(),
           x = Math.round((e.pageX - offset.left) / $(this).width() * 100),
           y = Math.round((e.pageY - offset.top) / $(this).height() * 100);
-          $(this).parent().find('p').empty().append(x + ", " + y);
-          newConfig.frames[$(this).data('frame')] = {
+          newConfig.frames[scrubber.val()] = {
             "pointer": {
               "style": hotSpotStyleForm.val(),
               "x": x + "%",
@@ -45,8 +83,8 @@ jQuery(document).ready(function($) {
             }
           };
           newConfigForm.val(JSON.stringify(newConfig, null, '\t'));
+          nextFrame();
         });
-
       }
     });
   });
